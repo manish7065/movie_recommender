22cd749ef8d1448f49aafc0a2b1223a3e76bbf99
manish <maimage95@gmail.com>
2023-05-15 08:36:33 +0530

    proper way

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -10,19 +10,31 @@ from src.components.data_transformation import DataTransformation,DataTransforma
 from src.components.model_trainer import ModelTrainer
 
 
-if __name__=='__main__':
-    obj=DataIngestion()
-    raw_data_path = obj.initiate_data_ingestion()
-    # print(f"{'**'*10} {raw_data_path}")
-    data_transformation = DataTransformation()
-    data_transformation.initaite_data_transformation(raw_data_path)
-    data_transformation_config = DataTransformationConfig()
-    transformed_data_path = data_transformation_config.transformed_data_file_path
-    print(f"Printing Ytansformed Data Path:- {transformed_data_path}")
+class TrainingPipeline:
+    def __init__(self) -> None:
+        pass
+        
+    def initiate_training(self):
+        try:
+            print('training pipeline starting')
+            logging.info(f"{'<<'*10}training pipeline starting.{'>>'*10}")
 
-    model_trainer=ModelTrainer()
-    model_trainer.initate_model_training(transformed_data_path)
+            obj=DataIngestion()
+            raw_data_path = obj.initiate_data_ingestion()
+            # print(f"{'**'*10} {raw_data_path}")
+            data_transformation = DataTransformation()
+            data_transformation.initaite_data_transformation(raw_data_path)
+            data_transformation_config = DataTransformationConfig()
+            transformed_data_path = data_transformation_config.transformed_data_file_path
+            print(f"Printing Ytansformed Data Path:- {transformed_data_path}")
 
+            model_trainer=ModelTrainer()
+            model_trainer.initate_model_training(transformed_data_path)
 
+            logging.info(f"{'<<'*10}training pipeline sucessfully completed.{'>>'*10}")
+
+        except Exception as e:
+            logging.info("Exception occured in prediction")
+            raise CustomException(e,sys)
 
 
