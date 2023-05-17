400bc2c10cd8902840cc3e2385580386a3e17efd
manish <maimage95@gmail.com>
2023-05-18 00:47:11 +0530

    make a home page routing

--- main.py
+++ main.py
@@ -6,29 +6,16 @@ from src.pipeline.training_pipeline import TrainingPipeline
 
 app = Flask(__name__)
 
-# def fetch_poster(movie_id):
-#     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-#     data = requests.get(url)
-#     data = data.json()
-#     poster_path = data['poster_path']
-#     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-#     return full_path
-
-# def recommend(movie):
-#     index = movies[movies['title'] == movie].index[0]
-#     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-#     recommended_movie_names = []
-#     recommended_movie_posters = []
-#     for i in distances[1:6]:
-#         # fetch the movie poster
-#         movie_id = movies.iloc[i[0]].movie_id
-#         recommended_movie_posters.append(fetch_poster(movie_id))
-#         recommended_movie_names.append(movies.iloc[i[0]].title)
-
-#     return recommended_movie_names,recommended_movie_posters
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    prediction_pipeline = PredictPipeline()
+    movies,model=prediction_pipeline.movie_list()
+    # print(movies)
     if request.method == 'POST':
         selected_movie = request.form['movie']
         recommended_movie_names,recommended_movie_posters = prediction_pipeline.recommend(selected_movie)
@@ -37,8 +24,8 @@ def home():
 
 @app.route('/train',methods = ['GET','POST'])
 def train():
-    # training_pipeline=TrainingPipeline()
-    # training_pipeline.initiate_training()
+    training_pipeline=TrainingPipeline()
+    training_pipeline.initiate_training()
     return jsonify({'message': 'model successfully trained.'})
         
 
@@ -46,8 +33,8 @@ if __name__ == '__main__':
     # training_pipeline = TrainingPipeline()
     # training_pipeline.initiate_training()
 
-    prediction_pipeline = PredictPipeline()
-    movies,model=prediction_pipeline.mivie_list()
+    # prediction_pipeline = PredictPipeline()
+    # movies,model=prediction_pipeline.mivie_list()
     # print(movies)
 
     # movies = pickle.load(open('movie_list.pkl', 'rb'))
